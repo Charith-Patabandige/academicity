124ed7df97e4a6bef4383c99b777d9023f9fc6e2
charith98patabandige <charithpriyadarshana925@gmail.com>
2024-06-13 15:11:26 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -9,7 +11,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Title subTitle='Our Program' title='What We Offer' />
         <Programs/>
+        <About/>
       </div>
     </div>
   )
