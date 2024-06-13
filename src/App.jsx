71114a38935f0bddb6beb4de295b9e948b341cdf
charith98patabandige <charithpriyadarshana925@gmail.com>
2024-06-13 13:30:24 +0530

    Programs Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+        <Programs/>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className='title'>
+        <p>Our Program</p>
+        <h2>What We Offer</h2>
+    </div>
+  )
+}
+
+export default Title
